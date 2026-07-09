3d2e34d89728b588197ba5af8262cc122e713ee7
Shivam Sinha <sinhashivam04@gmail.com>
2026-07-10 02:32:39 +0530

    Initial Setup

---
+++ App.js
@@ -0,0 +1,22 @@
+/**
+ * Sample React Native App
+ * https://github.com/facebook/react-native
+ *
+ * @format
+ */
+
+import {
+  SafeAreaProvider,
+  SafeAreaView,
+} from 'react-native-safe-area-context';
+
+function App() {
+
+  return (
+    <SafeAreaProvider>
+      <SafeAreaView />
+    </SafeAreaProvider>
+  );
+}
+
+export default App;
